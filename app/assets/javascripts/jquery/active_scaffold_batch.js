17634ec091af382ce2adb5b88f7b3b141b1c6623
scambra <sergio@enpijama.es>
2013-01-30 10:40:47 -1000

    update to jquery 1.9.0

--- app/assets/javascripts/jquery/active_scaffold_batch.js
+++ app/assets/javascripts/jquery/active_scaffold_batch.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function() {
-  jQuery('.batch-create-rows a').live('ajax:beforeSend', function(event, xhr, settings) {
+  jQuery(document).on('ajax:beforeSend', '.batch-create-rows a', function(event, xhr, settings) {
     var num_records = jQuery(this).closest('.batch-create-rows').find('input[name=num_records]').val();
     if (num_records) settings.url += (settings.url.indexOf('?') != -1 ? '&' : '?') + 'num_records=' + num_records;
     return true;
   });
-  jQuery('.multiple .form_record a.remove').live('click', function(event) {
+  jQuery(document).on('click', '.multiple .form_record a.remove', function(event) {
     event.preventDefault();
     var record = jQuery(this).closest('.form_record');
     record.prev('.form_record-errors').remove();
